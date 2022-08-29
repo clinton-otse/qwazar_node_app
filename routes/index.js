7fa714005561fdb2860f6d0af09e2b18f848f9ea
Clinton Otse <otseclinton60@gmail.com>
2022-08-29 14:18:00 +0100

    Updated

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  res.send('welcome to the api');
+});
+
+router.get('/form', function (req, res, next) {
   res.send('Google Form');
 });
 
